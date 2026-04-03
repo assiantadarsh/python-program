b607b31ed2a220719fd568d82ab3cefcc70d6eb1
Adarsh shukla <adarshbhai917963@gmail.com>
2026-04-03 23:14:07 +0530

    Add files via upload

---
+++ pattern-problem/16.diadonal_line.py
@@ -0,0 +1,15 @@
+# *
+#   *       
+#     *     
+#       *   
+#         * 
+n = int(input("Enter rows: "))
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
---
+++ pattern-problem/17.inverted_diagonal_line.py
@@ -0,0 +1,16 @@
+#           * 
+#         *   
+#       *     
+#     *       
+#   *
+# *
+n = int(input("Enter rows: "))
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i+j==n+1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
