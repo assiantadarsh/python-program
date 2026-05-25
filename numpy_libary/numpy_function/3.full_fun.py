0197775bae1a3fdaf6a96f310794198fc8bd4369
Adarsh shukla <adarshbhai917963@gmail.com>
2026-05-25 20:39:23 +0530

    Add files via upload

---
+++ numpy_libary/numpy_function/3.full_fun.py
@@ -0,0 +1,5 @@
+# np.full()
+# Use for ->  Creates an array of a given shape filled with a specific value.
+a = np.full((2,3),7)
+print(a)
+print("Dimension : ",a.ndim)
---
+++ numpy_libary/numpy_function/4.arange_fun.py
@@ -0,0 +1,4 @@
+# np.arange
+# Use for ->  Creates values in a range with a fixed step.
+a = np.arange(2,11,2)
+print(a)
