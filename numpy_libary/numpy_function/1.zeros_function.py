5089673e03e16e4d25d04c260958df6c0b38e3bd
Adarsh shukla <adarshbhai917963@gmail.com>
2026-05-25 20:38:13 +0530

    Add files via upload

---
+++ numpy_libary/numpy_function/1.zeros_function.py
@@ -0,0 +1,9 @@
+# np.zeros()
+# Use for ->  Creates an array filled with zeros. Useful for initialization
+a = np.zeros(3, dtype = int)
+print(a)
+print()
+
+# 2D array
+a = np.zeros((2,3),dtype = int)
+print(a)
---
+++ numpy_libary/numpy_function/2.ones_function.py
@@ -0,0 +1,8 @@
+# np.ones()
+# Use for -> Creates an array filled with ones.
+a = np.ones(4, dtype = int)
+print(a)
+print()
+print("2D array")
+a = np.ones((3,2),dtype = int)
+print(a)
