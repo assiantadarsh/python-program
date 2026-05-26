2587703ebe65b6e00caa0aa1ee25b24153e660a6
Adarsh shukla <adarshbhai917963@gmail.com>
2026-05-26 22:19:52 +0530

    Add files via upload

---
+++ numpy_libary/mathematical_function/add_fun.py
@@ -0,0 +1,5 @@
+# np.add()
+# Use ->  Adds two arrays element by element
+a1 = np.array([1,2,3,4,5])
+a2 = np.array([3,4,5,6,7])
+print(np.add(a1,a2))
---
+++ numpy_libary/mathematical_function/cbrt_fun.py
@@ -0,0 +1,4 @@
+# np.cbrt()
+# Use -> Finds the Cube root of each element.
+a1 = np.array([1,8,27,64,125])
+print(np.cbrt(a1))
