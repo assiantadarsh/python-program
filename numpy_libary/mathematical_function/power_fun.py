926d52437f6c8b610fea398825f5e85bb5de07b8
Adarsh shukla <adarshbhai917963@gmail.com>
2026-05-26 22:22:44 +0530

    Add files via upload

---
+++ numpy_libary/mathematical_function/power_fun.py
@@ -0,0 +1,4 @@
+# np.power()
+# use -> Raises each element to a given power.
+a1 = np.array([1,4,9,16,25])
+print(np.power(a1, 2))
---
+++ numpy_libary/mathematical_function/round_fun.py
@@ -0,0 +1,4 @@
+# np.round()
+# Use ->  Rounds decimal values to the given number of digits
+arr = np.array([1.234, 5.678])
+print(np.round(arr, 1))
