c843684aab3c9fac7f1007c34ee0402548b933cb
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-09 21:17:17 +0530

    Add files via upload

---
+++ list_problems/30.ele_smaller_than_aver.py
@@ -0,0 +1,31 @@
+n = int(input("Enter the size of the list : "))
+
+# Que -> Write a Python program to print all elements that are smaller than the average of the list.
+
+if(n <= 1):
+    print("Not possible")
+    exit()
+
+arr = []
+
+for i in range(n):
+    value = int(input("Enter values : "))
+    arr.append(value)
+
+print("The list is : ",arr)
+
+# Find Average
+
+sum = 0
+for i in range(n):
+    sum += arr[i]
+    
+average = sum / n
+
+temp = []
+
+for i in range(n):
+    if(average > arr[i]):
+        temp.append(arr[i])
+        
+print(f'Answer is : {temp}')
