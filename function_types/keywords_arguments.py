d79c13cfcef64a22ff3603b2916ed7b1bbc38ba4
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-28 00:49:52 +0530

    Add files via upload

---
+++ function_types/keywords_arguments.py
@@ -0,0 +1,7 @@
+def student_info(name , city , age):
+    print("Name:", name)
+    print("Age:", age)
+    print("City:", city)
+
+
+student_info(city="bhopal" , age = 34 , name='Adarsh')
---
+++ function_types/recursion_fun.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return factorial(n - 1) *n
+
+result = factorial(6)
+
+print("Factorial : ",result)
