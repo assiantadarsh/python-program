3c257cf2d7c34a17ad6f01aa4a19c3027ce69121
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-29 02:43:53 +0530

    Add files via upload

---
+++ oops_problem/class_and_objects_problem/1.problem.py
@@ -0,0 +1,8 @@
+class student:
+    name = "Adarsh"
+    age = 23
+    
+s = student()
+
+print(f'Name : {s.name}')
+print(f'Age : {s.age}')
---
+++ oops_problem/class_and_objects_problem/2.problem.py
@@ -0,0 +1,10 @@
+class car:
+    brand = "Toyota"
+    model = 2030
+    price = '4.5b'
+    
+c = car()
+
+print("Brand : " ,c.brand)
+print(f'Model : {c.model}')
+print(f'price : {c.price}')
