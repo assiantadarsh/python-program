b1bc40c34ad64a1080853cd02644c52d486b5a7b
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-29 02:45:05 +0530

    Add files via upload

---
+++ oops_problem/class_and_objects_problem/3.problem.py
@@ -0,0 +1,10 @@
+class mobile:
+    company = "I-Phone"
+    model = 2024
+    ram = '0.5KB'
+    
+m = mobile()
+
+print("Company : ",m.company)
+print("Model : ",m.model)
+print("Ram : ",m.ram)
---
+++ oops_problem/class_and_objects_problem/4.reactangle_area.py
@@ -0,0 +1,9 @@
+class reactangle:
+    length = 23.67
+    width = 67.34
+    
+    area = length * width
+    
+r = reactangle()
+
+print("Area : ", round(r.area , 2))
